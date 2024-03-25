0d38717f56c0f8d85864704b6d473b8b5c632d65
smnhff <117087063+smnhff@users.noreply.github.com>
2024-03-25 15:10:26 +0000

    small bugfix (#2)

--- fuzzware_pipeline/pipeline.py
+++ fuzzware_pipeline/pipeline.py
@@ -486,7 +486,6 @@ class Pipeline:
 
     # this checks if the currently defined checkpoint is hit 
     def checkpoint_progress(self, bbl_set):
-        tmp = self.current_checkpoint["checkpoint_target"]
         # did we find our checkpoint?
         return self.current_checkpoint and (self.current_checkpoint["checkpoint_target"] in bbl_set) and (
             # And no blacklist addresses found and all whitelist addresses in bbl set
